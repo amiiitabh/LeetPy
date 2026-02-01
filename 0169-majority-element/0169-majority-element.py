5d6cd163a1c97b1582fce1de1b10c80af652e0be
amiiitabh <149476166+amiiitabh@users.noreply.github.com>
2026-02-01 19:12:45 +0530

    Time: 0 ms (100%), Space: 21.2 MB (40.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        lst=[];st=set(nums)
-        sorted(nums)
-        for i in st:
-            lst.append(nums.count(i))
-        for  i in range(len(st)):
-            j=lst.index(max(lst))
-        lst2=list(st)
-        return (lst2[j])
-            
+        nums.sort()
+        return nums[len(nums)//2]
